0ff0735869eb8ffbf9638435954d9523036d48af
khannasid <khannasiddhant22@gmail.com>
2021-12-25 18:53:38 +0530

    Save link extention

---
+++ chrome-extention/script.js
@@ -0,0 +1,49 @@
+let myLeads = []
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById('tab-btn')
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  render(myLeads)
+})
+
+tabBtn.addEventListener('click', function () {
+  // chrome.tabs.query({ active: true, currentWindow: true}, function (tabs) {
+  // })
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
+})
+function render (leads) {
+  let listItems = ''
+  for (var i = 0; i < leads.length; i++) {
+    // listItems += "<li><a target='_blank' href= '" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    listItems += `<li>
+                    <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                    </a>
+                  </li>`
+  }// here in a tag, 'target' attribute specifies where to open the document.
+  // like '_blank' = opens link in new window.
+
+  ulEl.innerHTML = listItems
+
+// DOM menipulation has a cost! Use as less as possible.
+}
